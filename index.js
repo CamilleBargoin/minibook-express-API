a30827a2b7ea4954ae96e0e4c99072c04d39add3
CamilleBargoin <cbargoin@gmail.com>
2016-07-09 22:37:28 +0200

    test

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ var startServer = function() {
     app.use(function(req, res, next) {
       // res.header("Access-Control-Allow-Origin", "http://localhost:8080");
       res.header("Access-Control-Allow-Origin", "https://minibook-react.herokuapp.com");
+      res.header("Access-Control-Allow-Origin", "http://minibook-react.herokuapp.com");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
