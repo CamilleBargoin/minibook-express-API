b550333daeeaf5d579525713e5c60066a8b94473
CamilleBargoin <cbargoin@gmail.com>
2016-07-09 22:08:41 +0200

    test fix access-control-allow-origin for heroku

--- index.js
+++ index.js
@@ -71,7 +71,8 @@ var startServer = function() {
   console.log("start server");
 
     app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+      // res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+      res.header("Access-Control-Allow-Origin", "https://minibook-express.herokuapp.com");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
