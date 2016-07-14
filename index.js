466cde2241063dafd7033e561c95036a425adb4c
CamilleBargoin <cbargoin@gmail.com>
2016-07-14 20:02:52 +0200

    test

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ var session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 // var server = app.listen("3001");
-var server = app.listen();
+var server = app.listen(port);
 
 var io      = require('socket.io').listen(server);
 
