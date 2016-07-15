a7e3874ebacfcd065a232ec96ac0ffce39526cc3
CamilleBargoin <cbargoin@gmail.com>
2016-07-15 11:27:38 +0200

    test

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ var startServer = function() {
       // res.header("Access-Control-Allow-Origin", "https://minibook-react.herokuapp.com");
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      // res.header('Access-Control-Allow-Credentials', 'true');
+      res.header('Access-Control-Allow-Credentials', 'false');
       next();
     });
 
