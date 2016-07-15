b6c69ee9909e21a6a0478518845872cc39362fa1
CamilleBargoin <cbargoin@gmail.com>
2016-07-15 09:53:16 +0200

    test

--- index.js
+++ index.js
@@ -33,14 +33,14 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(app.listen(process.env.PORT));
 app.use(cookieParser());
 
-//  io.on("connection", function(socket) {
-//         console.log("NOUVELLE CONNEXION");
+ io.on("connection", function(socket) {
+        console.log("NOUVELLE CONNEXION");
 
-//         io.set('transports', [            // all transports (optional if you want flashsocket)
-//             'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'
-//         ]);
-//         io.set('origins', 'http://minibook-react.herokuapp.com:*');
-// });
+        io.set('transports', [            // all transports (optional if you want flashsocket)
+            'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'
+        ]);
+        io.set('origins', 'http://minibook-react.herokuapp.com:*');
+});
 
 
 app.use(express.static(__dirname + '/public'));
