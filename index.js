8f61d1bed407b5cf51317a3c19a67202701a4200
CamilleBargoin <cbargoin@gmail.com>
2016-07-17 17:23:32 +0200

    fix cors by environment

--- index.js
+++ index.js
@@ -84,11 +84,16 @@ var quotes = require('./routes/quotes');
 var startServer = function() {
 
   console.log("start server");
-
+console.log(process.env);
 
     app.use(function(req, res, next) {
-      // res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-      res.header("Access-Control-Allow-Origin", "http://minibook-react.herokuapp.com");
+      if (process.env.NODE_ENV) {
+        res.header("Access-Control-Allow-Origin", "http://minibook-react.herokuapp.com");
+      }
+      else {
+        res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+      }
+      // res.header("Access-Control-Allow-Origin", "http://minibook-react.herokuapp.com");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.header("Access-Control-Allow-Credentials", 'true');
       next();
